25acdebd6235251935885af897e18ddb2ff63069
yslee1126 <yslee1126@gmail.com>
2022-10-10 17:14:17 +0900

    router-dom 버전 변경, 리스트 실험

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
-import Home from "./routes/Home";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Detail from "./routes/Detail";
+// import Home from "./routes/Home";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import MainPage from './components/page/MainPage';
+import PostWritePage from './components/page/PostWritePage';
+import PostViewPage from './components/page/PostViewPage';
+import styled from "styled-components";
+
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/abot-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+        <MainTitleText>서비스 이름</MainTitleText>
+        <Routes>
+            <Route index element={<MainPage />} />
+            <Route path="post-write" element={<PostWritePage />} />
+            <Route path="post/:postId" element={<PostViewPage />} />
+        </Routes>
+    </BrowserRouter>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/abot-us">
+    //       <h1>Hello</h1>
+    //     </Route>
+    //     <Route path="/movie/:id">
+    //       <Detail />
+    //     </Route>
+    //     <Route path="/">
+    //       <Home />
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 }
 
