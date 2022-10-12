b8b07dc9a895d1dea2384ad515c1c30f730d41ba
yslee1126 <yslee1126@gmail.com>
2022-10-12 10:57:15 +0900

    홈리스트 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import MainPage from './components/page/MainPage';
+// import MainPage from './components/page/MainPage';
+import HomeList from './components/list/HomeList';
 import PostWritePage from './components/page/PostWritePage';
 import PostViewPage from './components/page/PostViewPage';
 import styled from "styled-components";
@@ -20,9 +21,10 @@ const MainTitleText = styled.p`
 function App() {
   return (
     <BrowserRouter>
-        <MainTitleText>서비스 이름</MainTitleText>
+        <MainTitleText>브랜드 R&D</MainTitleText>
         <Routes>
-            <Route index element={<MainPage />} />
+            {/* <Route index element={<MainPage />} /> */}
+            <Route index element={<HomeList />} />
             <Route path="post-write" element={<PostWritePage />} />
             <Route path="post/:postId" element={<PostViewPage />} />
         </Routes>
